834e6c13ffece2a55e2c049d4339403618a90fb1
JackieLin <jackie.lin@easyuse.com.tw>
2022-01-19 16:04:32 +0800

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
 import About from '../views/About.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
     {
@@ -20,6 +21,11 @@ const routes = [
         path: '/about',
         name: 'About',
         component: About,
+    },
+    {
+        path: '/:catchAll(.*)',
+        component: NotFound,
+        name: 'NotFound',
     },
 ]
 
